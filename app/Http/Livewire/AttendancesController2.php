906663a1130081c911962453fa07f1d5d9a3fa4f
branlyn <branlyn777@gmail.com>
2022-12-01 18:19:03 -0400

    cambiosjosuesitojueves

--- app/Http/Livewire/AttendancesController2.php
+++ app/Http/Livewire/AttendancesController2.php
@@ -2,18 +2,97 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Attendance;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AttendancesController2 extends Component
 {
+    use WithPagination;
+    public $reportType, $userId, $dateFrom, $dateTo, $horaentrada,$horaconformada, $componentName, $title, $fechahoy, $total, $archivo ,$verfiarchivo;
+    public $fechaf, $empleadoid, $entradaf, $salidaf, $prueba;
+
+    protected $pagination;
+
+    public function paginationView()
+    {
+        return 'vendor.livewire.bootstrap';
+    }
+
+    public function mount(){
+    
+        $this->reportType = 0;
+        $this->userId = 0;
+        $this->dateFrom = Carbon::parse(Carbon::now())->format('Y-m-d');
+        $this->dateTo = Carbon::parse(Carbon::now())->format('Y-m-d');
+        $this->componentName = "Fallo de Sistema";
+        $this->title = "algo";
+        $this->fechahoy = Carbon::parse(Carbon::now())->format('Y-m-d');
+        
+      // 
+    }
+
     public function render()
     {
-        $mmm = "";
+        $lista_asistencias = Attendance::join("employees as e","e.id","attendances.employee_id")
+        ->select("attendances.fecha as fecha_asistencia","attendances.entrada as entrada_asistencia","attendances.salida as salida_asistencia", "e.name as nombreemployees"
+        ,DB::raw('0 as dia'),DB::raw('0 as retraso'))
+        ->where("e.estado","Activo")
+        ->get();
+
+        foreach($lista_asistencias as $s)
+        {
+            $s->dia = $this->fecha_dia(Carbon::parse($s->fecha_asistencia)->format('D'));
+            
+        }
+
+
+
         return view('livewire.attendances.attendances2',[
-            'mmm' => $mmm,
+            'lista_asistencias' => $lista_asistencias,
         ])
         ->extends('layouts.theme.app')
         ->section('content');
         
+    }
+
+    //convertir de ingles a español
+    public function fecha_dia($dia)
+    {
+        switch ($dia) {
+            case 'Mon':
+                
+                return "Lunes";
+                break;
+            case 'Tue':
+                
+                return "Martes";
+                break;
+            case 'Wed':
+                
+                return "Miercoles";
+                break;
+            case 'Thu':
+
+                return "Jueves";
+                break;
+            case 'Fri':
+
+
+                return "Viernes";
+                break;
+            case 'Sat':
+
+                return "Sabado";
+                break;
+            case 'Sun':
+
+                return "Dimingo";
+                break;
+            default:
+                return "no se encontro resultado";
+        }
     }
 }
--- resources/views/livewire/attendances/attendances2.blade.php
+++ resources/views/livewire/attendances/attendances2.blade.php
@@ -3,17 +3,18 @@
         <div class="col-12 text-center">
             <p class="h1"><b>REPORTE DE ENTRADAS Y SALIDAS</b></p>
         </div>
-
-        <div class="row">
+    </div>
+        
+        {{-- <div class="row">
     
             <div class="col-12 col-sm-6 col-md-4">
                 <h6>Elige el Empleado</h6>
                 <div class="form-group">
                     <select wire:model="userId" class="form-control">
                         <option value="0">Todos</option>
-                        {{-- @foreach($employees as $employee)
+                        @foreach($employees as $employee)
                             <option value="{{$employee->id}}">{{$employee->name}}</option>
-                        @endforeach --}}
+                        @endforeach
                     </select>
                 </div>
             </div>
@@ -23,8 +24,8 @@
                 <div class="col-sm-3">
                     <h6>Fecha Desde</h6>
                     <div class="form-group" >
-                        {{-- <input @if ($reportType != 1) {{-- disabled --}}  @endif type="date" wire:model="dateFrom"
-                            class="form-control flatpickr" placeholder="Click para elegir"> --}}
+                         <input @if ($reportType != 1)   @endif type="date" wire:model="dateFrom"
+                            class="form-control flatpickr" placeholder="Click para elegir"> 
                     </div>
                 </div>
             </div>
@@ -34,8 +35,8 @@
                 <div class="col-sm-3 ">
                             <h6>Fecha Hasta</h6>
                             <div class="form-group">
-                               {{--  <input @if ($reportType != 1)  {{-- disabled --}} @endif type="date" wire:model="dateTo"
-                                    class="form-control flatpickr" placeholder="Click para elegir"> --}}
+                                <input @if ($reportType != 1)  @endif type="date" wire:model="dateTo"
+                                    class="form-control flatpickr" placeholder="Click para elegir"> 
                             </div>
                         </div>
             </div>
@@ -44,14 +45,66 @@
             <div class="col-12 col-sm-12 col-md-4 text-right">
                 <div class="col-sm-3">
                     <h2 style = "visibility: hidden">A</h2>
-                    
-                      {{--   <a class="btn btn-primary {{count($data) < 1? 'disabled' : ''}}"
-                        href="{{ url('report/excel' . '/' . $userId . '/' . $reportType. '/' . $dateFrom. '/' . $dateTo) }}"
-                        target="_blank">Exportar a Excel Salarios</a> --}}
 
                 </div>
             </div>
-    
+        </div> --}}
+
+  
+
+
+        <div class="table-5">
+            <table>
+                <thead> 
+                    <tr class="text-center">
+                        <th>N°</th>
+                        <th>Dia</th>
+                        <th>FECHA</th>
+                        <th>ENTREDA</th>
+                        <th>SALIDA</th>
+                        <th>NOMBRE</th>
+                        <th>RETRASO</th>
+                        <th>SALIDA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($lista_asistencias as $a)
+                    <tr>
+                        <td class="text-center">
+                            {{$loop->iteration}}
+                        </td>
+                        <td class="text-center">
+                            {{ ($a->dia) }}
+                        </td>
+                        <td class="text-center">
+                            {{-- {{ date("d/m/Y", strtotime($a->fecha)) }} --}}
+                            {{ Carbon\Carbon::parse($a->fecha_asistencia)->format('d/m/Y') }}
+                        </td>
+                        <td class="text-center">                          
+                            {{ Carbon\Carbon::parse($a->entrada_asistencia)->format(' H:i') }}
+                        </td>
+                        <td class="text-center">                          
+                            {{ Carbon\Carbon::parse($a->salida_asistencia)->format(' H:i') }}
+                        </td>
+                        <td class="text-center">
+                            {{ ($a->nombreemployees) }}
+                        </td>
+
+                        
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+          {{--   {{ $lista_asistencias->links() }} --}}
         </div>
-    </div>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function(){
+ 
+         window.livewire.on('show-modal', msg=>{
+             $('#theModal').modal('show')
+         });
+ 
+     });
+ </script>
